03a4de822f0ddbd5fa1b69ca8ef4a2fe9aaf8ab6
Nazareno Avendaño <98565832+nazarenoavendano@users.noreply.github.com>
2024-07-17 17:47:16 -0300

    Create sketch_fire_sensor_with_buzzer.c

---
+++ sketch_fire_sensor_with_buzzer.c
@@ -0,0 +1,28 @@
+int pinBuzz = 9;
+int pinFireSensor = A0;
+int sensorValue = 0; //This is important
+int shotValue = 500; //This is my trigger in the if statement
+int dt = 400;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinFireSensor, INPUT);
+  pinMode(pinBuzz, OUTPUT);
+}
+
+void loop() {
+  int sensorValue = analogRead(pinFireSensor);
+
+  Serial.print("Valor del sensor: ");
+  Serial.println(sensorValue);
+  Serial.println("---------------------");
+
+  if(sensorValue<shotValue) {
+    Serial.println("FUEGO FUEGO FUEGO!!!");
+    Serial.println("---------------------");
+    digitalWrite(pinBuzz, HIGH);
+    delay(300);
+    digitalWrite(pinBuzz, LOW);
+  }
+  delay(dt);
+}
